906142c31dc9274cad674b79ce182370b283137a
making <makingx@gmail.com>
2017-06-12 16:50:49 +0900

    add

---
+++ src/main/java/com/example/demoappsman/DemoAppsmanApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoappsman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppsmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppsmanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/demoappsman/DemoAppsmanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoappsman;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoAppsmanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
